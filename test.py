fc5705a3f259dee1a7a175aea7411cdee3b553e2
peter <win901116@gmail.com>
2026-05-29 07:21:52 +0800

    exp138: seed=251

--- test.py
+++ test.py
@@ -208,8 +208,8 @@ class myViT(nn.Module):
 # ╔══════════════════════════════════════════════════════════╗
 # ║  Part 9: 超參數  [MODIFIABLE]                            ║
 # ╚══════════════════════════════════════════════════════════╝
-torch.manual_seed(71)
-torch.cuda.manual_seed_all(71)
+torch.manual_seed(251)
+torch.cuda.manual_seed_all(251)
 
 model = myViT(
     input_size  = input_size,
